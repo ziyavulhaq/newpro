c1abb50b293bcdfa2e307c06b540fce0400f04be
ziyavulhaq <ziyavulhaq36@gmail.com>
2024-08-04 11:13:08 -0400

    new files is added

---
+++ new2.js
@@ -0,0 +1,7 @@
+var h1tag=document.querySelector("h1")
+var div=document.querySelector("div")
+var button=document.getElementById("but")
+button.addEventListener("click",function()
+ {
+  div.style.width="100%"
+ })
